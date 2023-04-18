4b6d320fd8e804074ac4b7b2e0aec24cc06bf89b
Madhuka Priyashan <madhuka164@gmail.com>
2023-04-18 22:24:00 +0530

    update

---
+++ project01/simpleroot2/src/main/java/com/example/simpleroot2/Simpleroot2Application.java
@@ -0,0 +1,13 @@
+package com.example.simpleroot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simpleroot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simpleroot2Application.class, args);
+	}
+
+}
---
+++ project01/simpleroot2/src/test/java/com/example/simpleroot2/Simpleroot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleroot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simpleroot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
