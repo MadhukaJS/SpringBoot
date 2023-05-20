cf039d629d5843a998edfbbb88f8fc521dafc02f
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-05-20 12:35:20 +0530

    update

---
+++ AiLabDetection_1/AiLabDetection/src/main/java/com/ailab/AiLabDetection/AiLabDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.ailab.AiLabDetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLabDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLabDetectionApplication.class, args);
+	}
+
+}
---
+++ AiLabDetection_1/AiLabDetection/src/test/java/com/ailab/AiLabDetection/AiLabDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ailab.AiLabDetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLabDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
